e0f2157e7b73e70069c617d8c741214bcf2e9b2b
Guilherme <guilhermenaooriginal@gmail.com>
2025-06-26 23:11:33 -0300

    cirado cadastro e livros

--- livros/views.py
+++ livros/views.py
@@ -6,8 +6,18 @@ def home(request):
     return render(request, 'home.html')
 
 def cadastrar_livro(request):
-    return render(request, 'cadastrar_livro.html')
+    livros = Livro.objects.all()
+    if request.method == 'POST':
+        form = LivroForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('livros')
+    else:
+        form = LivroForm
+    
+    return render(request, 'cadastrar_livro.html', {'form': form} )
 
 def livros(request): 
-    return render(request, 'livros.html')
+    livros = Livro.objects.all()
+    return render(request, 'livros.html', {'livros': livros})
 
