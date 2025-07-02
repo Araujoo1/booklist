545dc58e7818b81b3c07a82c487791ec524a3e7c
Guilherme <guilhermenaooriginal@gmail.com>
2025-07-02 17:55:44 -0300

    atualização self e banco

--- livros/models.py
+++ livros/models.py
@@ -5,3 +5,7 @@ class Livro(models.Model):
     autor = models.CharField(max_length=50)
     preco = models.DecimalField(max_digits=6, decimal_places=2)
     lido = models.BooleanField(default=False)
+
+    def __str__(self):
+        status = "Lido" if self.lido == True else "Não lido"
+        return f"{self.titulo} ({self.autor}) - R$ {self.preco} - {status} " 
